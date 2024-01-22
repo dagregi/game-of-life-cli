4637d9726e4213f65a9e9b79b0e362262447ffaa
dagregi <120724828+dagregi@users.noreply.github.com>
2024-01-22 13:31:14 +0000

    basic game of life

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use crossterm::event::{read, KeyCode, KeyEvent};
+use crossterm::queue;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+use crossterm::{
+    cursor::{Hide, MoveTo, Show},
+    event::{poll, Event},
+    execute,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal::{Clear, ClearType, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io::stdout;
+use std::io::Result;
+use std::io::Write;
+use std::time::Duration;
+
+mod game;
+
+fn main() -> Result<()> {
+    let mut game = game::Universe::new(5, 5);
+    let mut stdout = stdout();
+
+    enable_raw_mode()?;
+    game.set_cells(&[(2, 1), (2, 2), (2, 3)]);
+    execute!(
+        stdout,
+        EnterAlternateScreen,
+        SetForegroundColor(Color::White),
+        Hide
+    )?;
+
+    loop {
+        if poll(Duration::from_millis(500))? {
+            if let Event::Key(KeyEvent { code, .. }) = read()? {
+                if code == KeyCode::Esc {
+                    break;
+                }
+            }
+        } else {
+            queue!(stdout, Clear(ClearType::All))?;
+            let mut i = 0;
+            while let Some(line) = game.row_as_string(i) {
+                queue!(stdout, MoveTo(0, i as u16), Print(line))?;
+                i += 1;
+            }
+
+            queue!(
+                stdout,
+                MoveTo(0, (i + 1) as u16),
+                Print("Press Esc to exit...")
+            )?;
+            stdout.flush()?;
+            game.tick();
+        }
+    }
+    execute!(stdout, ResetColor, Show, LeaveAlternateScreen)?;
+    disable_raw_mode()?;
+    Ok(())
+}
